5f32cec10b5e4eadeca20cfa89121f34f1ebc8e4
bessemouelhi <bessem.mouelhi@outlook.fr>
2019-08-20 11:15:52 +0200

    add all gifs

--- src/main/java/com/bessemouelhi/giflib/controller/GifController.java
+++ src/main/java/com/bessemouelhi/giflib/controller/GifController.java
@@ -1,8 +1,16 @@
 package com.bessemouelhi.giflib.controller;
 
+import com.bessemouelhi.giflib.model.Gif;
+import com.bessemouelhi.giflib.repositories.GifRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDate;
+import java.util.List;
 
 /**
  * @author : Bessem MOUELHI
@@ -10,15 +18,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
  **/
 @Controller
 public class GifController {
+
+    @Autowired
+    private GifRepository gifRepository;
+
     @RequestMapping("/")
-    @ResponseBody
-    public String listGifs() {
-        return "List of Gifs";
+    public String listGifs(ModelMap modelMap) {
+        List<Gif> gifs = gifRepository.all();
+        modelMap.put("gifs", gifs);
+        return "home";
     }
 
-    @RequestMapping("/gif")
-    @ResponseBody
-    public String getGif() {
-        return "A Gif";
+    @RequestMapping("/gif/{name}")
+    public String gifDetail(@PathVariable String name, ModelMap modelMap) {
+        Gif gif = gifRepository.getByName(name);
+        modelMap.put("gif", gif);
+        return "gif-details";
     }
 }
